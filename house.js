418858770f33dcc21bbf3e62d076fcb0fae22977
Ken Duigraha Putra & Izhharuddin Syanmil <pair+kenduigraha.syanmil@hacktiv8.com>
2016-10-03 10:10:40 +0700

    release 0 done

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(components) {
+    this.address = components['address']
+    this.square_feet = components['square_feet']
+    this.num_bedrooms = components['num_bedrooms'] || 3
+    this.num_baths = components['num_baths'] || 2
+    this.cost = components['cost']  || 320000
+    this.down_payment = components['down_payment'] || 0.20
+    this.sold = components['sold'] || false
+    this.short_sale = components['short_sale']
+    this.has_tenants = components['has_tenants'] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -23,11 +23,27 @@ class House {
       this.sold = true
   }
 
-  down_payment() {
-    return cost * this.down_payment
+  dp() {
+    return this.cost * this.down_payment
   }
 
   to_s() {
-    return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. $ ${this.cost}`
+    // return `${obscure_address()} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
   }
 }
+
+var componentRumah = {
+  address : "jalan merdeka 10 Blok C Jakarta Pusat",
+  square_feet : 20,
+  num_bedrooms : 5,
+  num_baths : 3,
+  cost : 50000,
+  down_payment : 0.3,
+  sold : false,
+  short_sale : true,
+  has_tenants : false
+}
+
+var rumah = new House(componentRumah)
+console.log(rumah.to_s())
