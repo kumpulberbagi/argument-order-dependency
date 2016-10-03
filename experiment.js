89ece43a82db6e6cbdde4c447063b1b74180a22b
Juang Wiantoro & Elkana Lumbantobing <pair+broerjuang.elkana-ltobing@hacktiv8.com>
2016-10-03 10:30:31 +0700

    realease 0

---
+++ experiment.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let x = 2;
+let x = 5;
+
+
+console.log("x :", x, "y :", x);
--- house.js
+++ house.js
@@ -2,16 +2,16 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(komponen_rumah) {
+    this.address = komponen_rumah['address']
+    this.square_feet = komponen_rumah['square_feet']
+    this.num_bedrooms = komponen_rumah['num_bedrooms'] || 3
+    this.num_baths = komponen_rumah['num_baths'] || 2
+    this.cost = komponen_rumah['cost'] || 320000
+    this.down_payment = komponen_rumah['down_payment'] || 0.20
+    this.sold = komponen_rumah['sold'] || false
+    this.short_sale = komponen_rumah['short_sale']
+    this.has_tenants = komponen_rumah['has_tenants'] || false
   }
 
   obscure_address() {
@@ -31,3 +31,18 @@ class House {
     return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
   }
 }
+
+var parameter = {
+  address: "senayan",
+  cost: 12000
+};
+
+var rumahSenayan = new House(parameter);
+console.log(rumahSenayan.down_payment, rumahSenayan.has_tenants);
+// var params = new House(test)
+// console.log(params)
+// var rumahku = new House(test);
+
+// rumahku.down_payment = 12000;
+
+// console.log(rumahku.down_payment());
