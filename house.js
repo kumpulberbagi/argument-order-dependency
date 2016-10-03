f794eaf5425f2c6edc1793a21b1daf43a92b7116
Ahyana Rizky Pratama & YONI SETIAWAN <pair+ahyanarizky.yonisetiawan@devbootcamp.com>
2016-10-03 10:38:54 +0700

    Argument Order Done

--- house.js
+++ house.js
@@ -2,32 +2,57 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(property) {
+    this.address = property['address']
+    this.square_feet = property['square_feet']
+    this.num_bedrooms = property['num_bedrooms'] || 3
+    this.num_baths = property['num_baths'] || 2
+    this.cost = property['cost'] || 320000
+    this.down_payment = property['down_payment']|| 0.20
+    this.sold = property['sold'] || false
+    this.short_sale = property['short_sale']
+    this.has_tenants = property['has_tenants'] || false
   }
+
+
+  // constructor(components){
+  //   this.address = components['address']
+    // square_feet = components
+    // num_bedrooms = components || 3
+    // num_baths = components || 2
+    // cost = components || 32000
+    // down_payment = components || 0.20
+    // sold = components || false
+    // short_sale = components
+    // has_tenants = components || false
+
+  //}
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return ` Alamat : ${this.address.replace(/.{5}/g, '****')}`
   }
 
-  buy(money, good_credit) {
-    if (money >= down_payment && good_credit)
+  buy(money, cicilan) {
+    if(money >= this.cost || cicilan * 12 >= this.cost){
       this.sold = true
+      return `Rumah terjual`
+    }
+    return `Harga rumah : ${this.cost}, Uang yg diberikan : ${money}. Rumah tidak terjual`
   }
 
-  down_payment() {
-    return cost * this.down_payment
+  dp() {
+    return `Minimum DP : ${this.cost * this.down_payment}`
   }
 
   to_s() {
-    return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost} $`
   }
 }
+
+
+
+var rumah = new House({cost:50000, down_payment: 0.5, address: "Jakarta", square_feet: 120, short_sale: 50})
+console.log(rumah.obscure_address());
+console.log(rumah.dp());
+console.log(rumah.buy(5000, 100));
+console.log(rumah.to_s());
