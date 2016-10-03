af986221ff38f37de77e48fa9b56b72bd2caf74c
Ken Duigraha Putra & Izhharuddin Syanmil <pair+kenduigraha.syanmil@hacktiv8.com>
2016-10-03 10:17:02 +0700

    release 1 done

--- car.js
+++ car.js
@@ -3,7 +3,8 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights'])
+    console.log(options)
+    if (options && options.hasOwnProperty('headlights'))
       this.headlights = options['headlights']
     else
       this.headlights = true
@@ -22,7 +23,5 @@ herbie.start_car()
 console.log(herbie.headlights) // true
 
 // But what happens when we want to start without headlights?
-herbie.start_car({
-  headlights: false
-})
+herbie.start_car({headlights: "lampu"})
 console.log(herbie.headlights) // true ...KOK BISA?!
