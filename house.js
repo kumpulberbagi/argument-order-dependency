465dbb69eb85bd05f4de5d373759dbb40aa61670
Alessandro C <alessandro.christopher@gmail.com>
2016-10-14 11:44:22 +0700

    Submitted

--- house.js
+++ house.js
@@ -2,16 +2,16 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(arg) {
+    this.address = arg['address']
+    this.square_feet = arg['square_feet']
+    this.num_bedrooms = arg['num_bedrooms'] || 3
+    this.num_baths = arg['num_baths'] || 2
+    this.cost = arg['cost'] || 320000
+    this.down_payment = arg['down_payment'] || 0.20
+    this.sold = arg['sold'] || false
+    this.short_sale = arg['short_sale']
+    this.has_tenants = arg['has_tenants'] || false
   }
 
   obscure_address() {
@@ -31,3 +31,16 @@ class House {
     return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
   }
 }
+
+var house = []
+house['square_feet'] = 200
+house['sold'] = false
+house['address'] = 'Pondok Indah'
+house['num_bedrooms'] = 2
+house['num_baths'] = 2
+house['cost'] = 255000
+house['down_payment'] = 0.20
+house['short_sale'] = true
+house['has_tenants'] = true
+let rumah = new House(house)
+console.log(rumah);
